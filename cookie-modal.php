4b0f6336f19188f0fc256a2b1974a1abbd472335
dirtyfork <129858066+dirtyfork@users.noreply.github.com>
2023-10-11 12:50:01 +0100

    Create cookie-modal.php

---
+++ cookie-modal.php
@@ -0,0 +1,9 @@
+<div id="cookie-modal" class="cookie-modal">
+    <div class="cookie-modal-content">
+        <p>We use cookies to analyze website traffic and optimize your experience. <a href="#">Learn more</a></p>
+        <form method="post">
+            <button type="submit" name="ga4_cookie_consent" value="allow" class="allow-button">Allow cookies</button>
+            <button type="button" class="deny-button" onclick="closeCookieModal()">Deny cookies</button>
+        </form>
+    </div>
+</div>
